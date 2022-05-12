bf4c8e71a52b203286f68e9bc219f9d578d446c2
SalyginVS <salyginvs@gmail.com>
2022-05-12 14:59:21 +0300

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ const scripts = () => {
       'app/js-dev/**/*.js'])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
-    .pipe(dest('./app/js/'))
+    .pipe(dest('./app/s-dev/js/'))
     .pipe(browserSync.stream())
 }
 
